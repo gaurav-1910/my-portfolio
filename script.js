21799c5b7a6b5a31349679edd598fa7b48c476ec
gauravsharma <gauravsharma@truckstop.com>
2022-01-19 02:44:35 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+document.querySelector('.cross').style.display ='none';
+document.querySelector('.ham').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo'))
+    {
+        document.querySelector('.ham').style.display = 'inline'
+        document.querySelector('.cross').style.display = 'none'
+    }
+    else
+    {
+        document.querySelector('.ham').style.display= 'none'
+        setTimeout(()=>{
+            document.querySelector('.cross').style.display= 'inline'
+        },350);
+    }
+
+})
+
+document.querySelector('.cross').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    document.querySelector('.ham').style.display= 'inline';
+
+})
